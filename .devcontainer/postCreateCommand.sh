df22bf7632db3200e5c4d582383a66ad8e7e1a7a
Nobuchika Tanaka <codequokka@gmail.com>
2024-03-22 21:33:19 +0000

    Add initial settings

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 # postCreateCommand.sh
 
-echo "Starting setup..."
+echo "START Install"
 
 sudo chown -R vscode:vscode .
 
-# Poetry
-# poetry config virtualenvs.in-project true
-# poetry install
+poetry config virtualenvs.in-project true
+poetry install
 
-# Pip
-python3 -m venv .venv
-. .venv/bin/activate
-pip install -r requirements-dev.txt
-
-echo "Finished setup"
+echo "FINISH Install"
