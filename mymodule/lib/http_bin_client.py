653f7b4059e03f7d552393a62174c5ec4d73874d
Nobuchika Tanaka <codequokka@gmail.com>
2024-03-23 02:57:25 +0000

    Refactoring

--- mymodule/lib/http_bin_client.py
+++ mymodule/lib/http_bin_client.py
@@ -1,5 +1,22 @@
+from __future__ import annotations
+
 import requests
 from requests import Response
+
+from typing import Any, Callable, NoReturn
+
+
+def handle_request_exceptions(func: Callable[..., Response]) -> Callable[..., Response]:
+    def wrapper(*args: Any, **kwargs: Any) -> Response | NoReturn:
+        try:
+            response = func(*args, **kwargs)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            raise
+
+    return wrapper
 
 
 class HttpBinClient(object):
@@ -8,5 +25,10 @@ class HttpBinClient(object):
     def __init__(self) -> None:
         self.session = requests.Session()
 
+    def _make_request(self, endpoint: str) -> Response:
+        url = f"{self.BASE_URL}/{endpoint}"
+        return self.session.get(url)
+
+    @handle_request_exceptions
     def get_ip(self) -> Response:
-        return self.session.get(f"{self.BASE_URL}/ip")
+        return self._make_request("i")
--- tests/test_http_bin_client.py
+++ tests/test_http_bin_client.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock, patch
 
 import pytest
+import requests
 
 from mymodule.lib.http_bin_client import HttpBinClient
 
@@ -11,9 +12,35 @@ def mock_http_bin_client() -> Mock:
 
 
 @patch("mymodule.lib.http_bin_client.HttpBinClient.get_ip")
-def test_get_ip_normal(mock_http_bin_client: Mock) -> None:
+def test_get_ip_success(mock_http_bin_client: Mock) -> None:
     want = {"origin": "127.0.0.1"}
     mock_http_bin_client.return_value = want
+
     client = HttpBinClient()
     got = client.get_ip()
+
     assert got == want
+
+
+@patch("mymodule.lib.http_bin_client.HttpBinClient.get_ip")
+def test_get_ip_fail(mock_http_bin_client: Mock) -> None:
+    mock_http_bin_client.side_effect = Exception("Request failed")
+
+    client = HttpBinClient()
+    with pytest.raises(Exception) as exc_info:
+        client.get_ip()
+
+    assert str(exc_info.value) == "Request failed"
+
+
+# @patch("mymodule.lib.http_bin_client.HttpBinClient.get_ip")
+# def test_get_ip_failure_404(mock_http_bin_client: Mock) -> None:
+#     # モックが404応答を返すように設定
+#     mock_http_bin_client.return_value = {"status_code": 404, "error": "Not Found"}
+
+#     client = HttpBinClient()
+#     with pytest.raises(requests.exceptions.RequestException) as exc_info:
+#         client.get_ip()
+
+#     # 404応答が返されることを確認
+#     assert str(exc_info.value) == "HTTP request returned 404: Not Found"
