d8ead6122b2847f7c38ed00f990d3740639e03a5
Nobuchika Tanaka <codequokka@gmail.com>
2024-03-23 11:07:27 +0900

    Refactoring

---
+++ tests/test_http_bin_client.py
@@ -0,0 +1,19 @@
+from unittest.mock import Mock, patch
+
+import pytest
+
+from mymodule.lib.http_bin_client import HttpBinClient
+
+
+@pytest.fixture
+def mock_http_bin_client() -> Mock:
+    return Mock(spec=HttpBinClient)
+
+
+@patch("mymodule.lib.http_bin_client.HttpBinClient.ip")
+def test_get_ip_normal(mock_http_bin_client: Mock) -> None:
+    want = {"origin": "127.0.0.1"}
+    mock_http_bin_client.return_value = want
+    client = HttpBinClient()
+    got = client.ip()
+    assert got == want
